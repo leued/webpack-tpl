ab4b1a1b447cacf14d2fc8d19d859ec16276e1b0
System Administrator <leued2015@outlook.com>
2018-04-19 19:14:04 +0800

    first version

---
+++ config/c.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const root = path.join(__dirname + '/../');
+const fs = require('fs');
+const Config =  {
+	root ,
+	path
+}
+
+module.exports = Config;
+
--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,20 +1,28 @@
-const path = require('path');
+const $c = require('./c.js')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-
 module.exports = {
+	mode: 'production',
     entry: {
         app: './src/index.js'
     },
+    devServer: {
+	        contentBase: $c.root + 'dist',
+	        compress: false,
+	        port: 8089,
+	        inline: false
+	    },
     plugins: [
+    	
         new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
             filename: 'index.html',
-            template: 'index.html'
+            template: './index.html',
+            favicon : './favicon.ico'
         })
     ],
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: $c.root + 'dist'
     }
 };

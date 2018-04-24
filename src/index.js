90f668d1da9dae6d9b979e8226e6fdcf55c4b4d4
System Administrator <leued2015@outlook.com>
2018-04-24 10:51:09 +0800

    add

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
-function component() {
-  var element = document.createElement('div');
+import {a} from './test/test.js';
 
-  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  return element;
-}
-
-document.body.appendChild(component());
+console.log(a)
